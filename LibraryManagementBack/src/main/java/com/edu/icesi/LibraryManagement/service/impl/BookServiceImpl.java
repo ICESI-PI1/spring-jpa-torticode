034054a333421c772240047604645b7438e2c8a0
CarlosJaBr <carjabo12@gmail.com>
2023-11-20 16:56:42 -0500

    feat_add_book_service_JPA

--- LibraryManagementBack/src/main/java/com/edu/icesi/LibraryManagement/service/impl/BookServiceImpl.java
+++ LibraryManagementBack/src/main/java/com/edu/icesi/LibraryManagement/service/impl/BookServiceImpl.java
@@ -1,11 +1,14 @@
 package com.edu.icesi.LibraryManagement.service.impl;
 
+import com.edu.icesi.LibraryManagement.persistence.model.Author;
 import com.edu.icesi.LibraryManagement.persistence.model.Book;
 import com.edu.icesi.LibraryManagement.persistence.repository.IBookRepository;
 import com.edu.icesi.LibraryManagement.service.IBookService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BookServiceImpl implements IBookService {
 
@@ -16,31 +19,51 @@ public class BookServiceImpl implements IBookService {
     };
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return bookRepository.findAll();
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        return Optional.empty();
+        return bookRepository.findById(id);
     }
 
     @Override
     public Book saveBook(Book book) {
-        return null;
+        return bookRepository.save(book);
     }
 
     @Override
     public Boolean uploadBook(Long id, Book book) {
-        return null;
+
+        boolean flag = false;
+        Optional<Book> oldBook = findById(id);
+        if(oldBook.isPresent()){
+            saveBook(book);
+            flag=true;
+        }else{
+            throw new NoSuchElementException("No book found with the given ID: " + id);
+        }
+
+        return flag;
     }
 
     @Override
     public Boolean deleteBook(Long id) {
+
+        boolean flag = false;
+        Optional<Book> book = findById(id);
+        if(book.isPresent()){
+            flag=true;
+            bookRepository.delete(book.get());
+        }else{
+            throw new NoSuchElementException("No book found with the given ID: " + id);
+        }
+
         return null;
     }
-
     @Override
     public List<Book> getBooksbyAuthor(Long idAuthor) {
-        return null;
+        return getAllBooks().stream().filter(p -> p.getAuthor().getId().equals(idAuthor)).
+                collect(Collectors.toList());
     }
 }
