5a286b49f256a64c229f580678a577a5a81d9b8f
CarlosJaBr <carjabo12@gmail.com>
2023-11-20 16:42:04 -0500

    feat_add_author_service_JPA

--- LibraryManagementBack/src/main/java/com/edu/icesi/LibraryManagement/service/impl/BookServiceImpl.java
+++ LibraryManagementBack/src/main/java/com/edu/icesi/LibraryManagement/service/impl/BookServiceImpl.java
@@ -1,4 +1,46 @@
 package com.edu.icesi.LibraryManagement.service.impl;
 
-public class BookServiceImpl {
+import com.edu.icesi.LibraryManagement.persistence.model.Book;
+import com.edu.icesi.LibraryManagement.persistence.repository.IBookRepository;
+import com.edu.icesi.LibraryManagement.service.IBookService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class BookServiceImpl implements IBookService {
+
+    private IBookRepository bookRepository;
+
+    public BookServiceImpl(IBookRepository bookRepository){
+        this.bookRepository=bookRepository;
+    };
+    @Override
+    public List<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Book saveBook(Book book) {
+        return null;
+    }
+
+    @Override
+    public Boolean uploadBook(Long id, Book book) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteBook(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksbyAuthor(Long idAuthor) {
+        return null;
+    }
 }
